8cd7f786fdcc7e2072956f56e631cc2a2067f0d5
jimmydin7 <turnspeaker@gmail.com>
2025-12-17 21:57:06 +0200

    sqlite db schema, gitignore, venv, requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
 import os
+import sqlite3
+
+db_path = "data/reviews.db"
+db_schema = """
+    CREATE TABLE IF NOT EXISTS reviews (
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        name TEXT NOT NULL
+        review TEXT NOT NULL 
+    )
+"""
+
 
 app = Flask(__name__)
+
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+cursor.execute(db_schema)
+conn.commit()
+conn.close()
 
 @app.route('/')
 def index():
