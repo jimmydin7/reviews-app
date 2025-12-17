40ba551cbaa2ae040bc1f728ff2c5d196ba6e4fe
jimmydin7 <turnspeaker@gmail.com>
2025-12-17 21:46:08 +0200

    added form handling (not saving to db)

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,20 @@
 const form = document.getElementById('reviewForm');
 const reviewsContainer = document.getElementById('reviews');
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault()
+    
+    const name = form.elements.name.value;
+    const review = form.elements.review.value;
+
+    const item = document.createElement("p");
+    
+    item.textContent = `Review from ${name}: ${review}`;
+    reviewsContainer.appendChild(item);
+
+    form.reset();
+
+})
+
+
